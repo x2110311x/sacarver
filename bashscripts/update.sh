abe948c8a30593d4ceb160d32c9df9a40837ccac
Alex Sweeney <x2110311x@gmail.com>
2018-10-29 21:17:23 -0400

    git stuff
    changed pull to merge

--- bashscripts/update.sh
+++ bashscripts/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /bots/sacarver
-git reset --hard origin/master
-git pull git@github.com:x2110311x/sacarver.git
+git fetch git@github.com:x2110311x/sacarver.git
 chown root:root /bots/sacarver/bashscripts/*
 chmod 777 /bots/sacarver/bashscripts/*
