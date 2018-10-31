419633e06256125091c6ad882f9dc1ecad8f7e8b
Alex Sweeney <x2110311x@gmail.com>
2018-10-30 22:00:58 -0400

    Forgot an import

--- sacarver.py
+++ sacarver.py
@@ -8,7 +8,7 @@ from threading import Timer
 import csv
 import string
 from google_images_download import google_images_download
-from include import config, utilities, hackerman
+from include import hackerman, utilities, config, txtutils, imageutils
 
 #General variables for code execution
 starttime = time()
