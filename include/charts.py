3c7f90085376699a150ccaa7d16b37934872ab33
Alex Sweeney <x2110311x@gmail.com>
2018-11-11 19:09:27 -0500

    whoops

--- include/charts.py
+++ include/charts.py
@@ -38,6 +38,7 @@ def messyesterday(imgname):
     plt.ylabel('Number of messages')
     plt.title('Messages per hour yesterday')
     plt.savefig(imgname)
+    plt.close()
 
 def mymessyesterday(imgname,ID):
     times = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm"]
@@ -61,3 +62,4 @@ def mymessyesterday(imgname,ID):
     plt.ylabel('Number of messages')
     plt.title('Messages per hour yesterday')
     plt.savefig(imgname)
+    plt.close()
