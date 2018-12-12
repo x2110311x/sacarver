d2e673931e267e17031918a446b9321346b8d27a
Alex Sweeney <x2110311x@gmail.com>
2018-12-11 22:12:34 -0500

    hhhh

--- sacarver.py
+++ sacarver.py
@@ -67,7 +67,7 @@ async def on_message(message):
 		
 	if message.timestamp.timestamp() >= 1544584324:
 		x2 = await client.get_user_info("207129652345438211")
-		if global sendx2 == False:
+		if sendx2 == False:
 			await client.send_message(x2,"Hi")
 			global sendx2 = True
 		
