ff18efdb1533aaaf413b73ffc3eeccca5271e2b1
Alex Sweeney <x2110311x@gmail.com>
2019-02-21 13:50:44 -0500

    Continuation of rewrite

--- sacarver.py
+++ sacarver.py
@@ -1,15 +1,17 @@
 # ********************************************************* #
+# Name: sacarver.py
 # Author: Alex Sweeney (x2110311x)
-# Desc :This file is the main script for running Sacarver
+# Desc: This file is the main script for running Sacarver
 # ********************************************************* #
 
 # Included Libraries #
 import discord
-import datetime
 import random
 import os
 import csv
 from time import time, sleep
+from pathlib import Path
+from datetime import datetime, timedelta
 from discord.ext import commands
 from google_images_download import google_images_download
 from include import hackerman, utilities, config, txtutils, charts
@@ -55,3 +57,62 @@ async def on_member_join(member):
 async def time(ctx):
     intCurEpoch = int(ctx.timestamp.timestamp())
     ctx.send("The current epoch is {intCurEpoch}")
+
+@bot.command()
+@commands.has_role(config.roles['staff'])
+async def totalmessagesyesterday(ctx):
+    dtYesterday = datetime.now() - timedelta(days=1)
+    intYesterday = int(datetime.combine(dtYesterday, datetime.max.time()).timestamp())
+    embedStartGen = discord.Embed(title=None, type="rich", colour=0xF4DA01)
+    embedStartGen.set_footer(name=client.user.name,
+                             icon_url=client.user.avatar_url)
+    embedStartGen.add_field(name="Generating image",
+                            value="This may take some time")
+    msgGenStatus = await ctx.send(embed=embedStartGen)
+    imgPath = Path("/bots/sacarver/images/" + str(intYesterday) + ".png")
+    if imgPath.is_file():
+        imgPath = "/bots/sacarver/images/" + str(intYesterday) + ".png"
+    else:
+        imgPath = "/bots/sacarver/images/" + str(intYesterday) + ".png"
+        charts.messyesterday(imgPath)
+    embedResult = discord.Embed(title="Messages per hour yesterday for the server",
+                                type="rich", colour=0xF4DA01)
+    embedResult.set_footer(name=client.user.name,
+                           icon_url=client.user.avatar_url)
+    imgToEmbed = discord.File(imgPath, name="image.png")
+    embedResult.set_image(url="attachment://image.png")
+    await msgGenStatus.delete()
+    await ctx.send(file=imgToEmbed, embed=embedResult)
+
+
+@bot.command()
+@commands.has_role(config.roles['staff'])
+async def usermessagesyesterday(ctx, *args, **kwargs):
+    userForMessages = kwargs.get('user',ctx.author)
+    dtYesterday = datetime.now() - timedelta(days=1)
+    intYesterday = int(datetime.combine(dtYesterday, datetime.max.time()).timestamp())
+    embedStartGen = discord.Embed(title=None, type="rich", colour=0xF4DA01)
+    embedStartGen.set_footer(name=client.user.name,
+                             icon_url=client.user.avatar_url)
+    embedStartGen.add_field(name="Generating image",
+                            value="This may take some time")
+    msgGenStatus = await ctx.send(embed=embedStartGen)
+    imgPath = Path("/bots/sacarver/images" + str(intYesterday) +
+                   userForMessages.id + ".png")
+    if imgPath.is_file():
+        imgPath = str("/bots/sacarver/images" + str(intYesterday) +
+                      userForMessages.id + ".png")
+    else:
+        imgPath = str("/bots/sacarver/images" + str(intYesterday) +
+                      userForMessages.id + ".png")
+        charts.mymessyesterday(imgPath)
+    imgPath = "/bots/sacarver/images" + str(int(time())) + userForMessages.id \
+        + ".png"
+    embedResult = discord.Embed(title="Messages per hour yesterday for {ctx.author.name}",
+                                type="rich", colour=0xF4DA01)
+    embedResult.set_footer(name=client.user.name,
+                           icon_url=client.user.avatar_url)
+    imgToEmbed = discord.File(imgPath, name="image.png")
+    embedResult.set_image(url="attachment://image.png")
+    await msgGenStatus.delete()
+    await ctx.send(file=imgToEmbed, embed=embedResult)
