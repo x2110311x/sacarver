f6e08a447d94015947ed6cda252a00d4fd3d4b76
Alex Sweeney <x2110311x@gmail.com>
2022-08-21 14:21:44 -0400

    Add command placeholders

---
+++ commands/fun/fitcheck.js
@@ -0,0 +1,12 @@
+module.exports = {
+    builder: function (SlashCommandBuilder){
+        SlashCommandBuilder.addSubcommand(subcommand =>
+            subcommand
+                .setName('fitcheck')
+                .setDescription('Fit check!'))
+        return SlashCommandBuilder
+    },
+    execute: async function(interaction){
+        await interaction.reply({content: "This command is still a work in progress", ephemeral: true});
+    }
+}
