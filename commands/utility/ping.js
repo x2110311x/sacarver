d29253d9fd5152ad4680cc353689149470fef615
Alex Sweeney <x2110311x@gmail.com>
2022-09-02 20:35:34 -0400

    Fix ping command to be a subcommand

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,25 +1,27 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Checks the Bot Latency and API heartbeat'),
+	builder: function (SlashCommandBuilder){
+		SlashCommandBuilder.addSubcommand(subcommand =>
+				subcommand
+						.setName('ping')
+						.setDescription('Check the bot latency'))
+		return SlashCommandBuilder
+	},
 	async execute(interaction) {
 		const before = interaction.createdAt.getTime();
 		const heartbeat = Math.floor(interaction.client.ws.ping);
-		await interaction.reply('Pinging...', { fetchReply: true })
-			.then(() => {
-				const after = Date.now();
-				const ping = Math.abs(after - before);
-				const pingEmbed = new MessageEmbed()
-					.setColor('#d5b052')
-					.addFields(
-						{ name: 'API Heartbeat', value: `${heartbeat}ms` },
-						{ name: 'Bot Latency', value: `${ping}ms` },
-					);
-				interaction.editReply({ content:'Pong!', embeds: [pingEmbed] });
-			});
+		await interaction.reply('Pinging...', { fetchReply: true });
+		const after = Date.now();
+		const ping = Math.abs(after - before);
+		const pingEmbed = new EmbedBuilder()
+			.setColor('#d5b052')
+			.addFields(
+				{ name: 'API Heartbeat', value: `${heartbeat}ms` },
+				{ name: 'Bot Latency', value: `${ping}ms` },
+			);
+		await interaction.editReply({ content:'Pong!', embeds: [pingEmbed] });
 	},
 };
