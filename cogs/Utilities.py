176e4d97fe9297dd9f498afebc31af5851f9fb05
Alex Sweeney <x2110311x@gmail.com>
2023-09-06 09:07:09 -0400

    Throw the update_status stuff into a try except

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -64,18 +64,21 @@ class Utilities(commands.Cog, name="Utility Commands"):
     
     @tasks.loop(seconds=30.0)
     async def update_status(self):
-        ping = int(self.bot.latency * 1000)
-        connectionstatus = not (self.bot.is_closed())
-        if connectionstatus:
-            status = "up"
-            msg = "OK"
-        else:
-            status = "down"
-            msg="Lost\%20\connection\%20to\%20Discord"
-        statusurl = f"{config['statusurl']}?status={status}&ping={ping}&msg={msg}"
-        print(statusurl)
-        async with aiohttp.ClientSession() as session:
-            await session.get(statusurl)
+        try:
+            ping = int(self.bot.latency * 1000)
+            connectionstatus = not (self.bot.is_closed())
+            if connectionstatus:
+                status = "up"
+                msg = "OK"
+            else:
+                status = "down"
+                msg="Lost\%20\connection\%20to\%20Discord"
+            statusurl = f"{config['statusurl']}?status={status}&ping={ping}&msg={msg}"
+            print(statusurl)
+            async with aiohttp.ClientSession() as session:
+                await session.get(statusurl)
+        except:
+            print("Error pushing connection status")
 
     @update_status.before_loop
     async def before_update_status(self):
