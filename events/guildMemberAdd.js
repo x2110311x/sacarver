44ef759ca7c2d6d1652a1c1aadc445b82f9daf74
Alex Sweeney <x2110311x@gmail.com>
2024-10-15 21:56:00 -0400

    Remove pieces not needed for the event

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'guildMemberAdd',
-	once: false,
-	async execute(member, client) {
-		client.log.debug('User join');
-		client.user.setActivity(`${member.guild.memberCount} members`, { type: 'WATCHING' });
-	},
-};
--- events/guildMemberRemove.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'guildMemberRemove',
-	once: false,
-	async execute(member, client) {
-		client.log.debug('User leave');
-		client.user.setActivity(`${member.guild.memberCount} members`, { type: 'WATCHING' });
-	},
-};
