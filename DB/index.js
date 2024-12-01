75bb4ae3659c92ea8a3f4b51a160ec51f2227c22
Alex Sweeney <x2110311x@gmail.com>
2024-12-01 18:54:57 -0500

    Create DB singleton class

---
+++ DB/index.js
@@ -0,0 +1,24 @@
+let instance = null;
+const { Sequelize } = require('sequelize');
+const initModels = require("./models/init-models");
+const config = require('../config.json');
+class SacarverDB {
+    constructor() {
+        const sequelize = new Sequelize(config.db.DBName, config.db.user, config.db.pass, {
+            host: config.db.host,
+            port: config.db.port,
+            dialect: 'mysql'
+        });
+        initModels(sequelize);
+        this.db =initModels(sequelize);
+    }
+    
+    static getInstance() {
+        if(!instance) {
+            instance = new SacarverDB();
+        }
+        return instance;
+    }
+}
+
+module.exports = SacarverDB;
