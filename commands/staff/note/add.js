d12c44d3e3bd94341fab4ad4c3ca8abf15bfe394
Alex Sweeney <x2110311x@gmail.com>
2024-12-01 20:08:09 -0500

    Handle storing note in the database

--- commands/staff/note/add.js
+++ commands/staff/note/add.js
@@ -1,6 +1,177 @@
-const { EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonBuilder, ButtonStyle } = require('discord.js');
 const crypto = require("crypto");
-const wait = require('node:timers/promises').setTimeout;
+
+async function sendModal(interaction){
+  const modalID = crypto.randomBytes(16).toString("hex");
+  const modal = new ModalBuilder()
+    .setCustomId(modalID)
+    .setTitle("Enter note details");
+
+  const noteInput = new TextInputBuilder()
+    .setCustomId('note')
+    .setLabel("Note")
+    .setRequired(true)
+    .setStyle(TextInputStyle.Paragraph);
+  
+    const messageLinkInput = new TextInputBuilder()
+      .setCustomId('link')
+      .setLabel("Message Link")
+      .setValue("N/A")
+      .setRequired(false)
+      .setStyle(TextInputStyle.Paragraph);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(noteInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(messageLinkInput);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+    await interaction.showModal(modal);
+
+    const collectorFilter = i => (i.user.id === interaction.user.id && i.customId === modalID);
+
+    interaction.awaitModalSubmit({ time: 600_000, collectorFilter })
+      .then(i => {
+        catchModal(interaction, i)
+      }).catch(err => {
+        interaction.client.log.warn({message: `Error with modal response for /staff note add`, error:err});
+      });
+}
+
+async function catchModal(interaction, newInteraction){
+  let client = interaction.client;
+  newInteraction.deferReply();
+
+  var severity = interaction.options.getString('severity');
+  var user = interaction.options.getUser('user');
+  if (user == null){
+      user = interaction.options.getString('userid');
+  } else {
+    user = user.id;
+  }
+  const note = newInteraction.fields.getTextInputValue('note');
+  const msgLink = newInteraction.fields.getTextInputValue('link');
+
+  var data = {
+    "user": user,
+    "severity": severity,
+    "note": note,
+    "msgLink": msgLink,
+    "dateAdded": Math.floor(newInteraction.createdTimestamp/1000),
+    "noter": interaction.member.id
+  }
+  
+  const noteEmbed = new EmbedBuilder()
+  .setColor(0xff0000)
+  .setTitle("New Staff Note")
+  .setDescription("Please review, and then click Submit or Cancel")
+  .addFields(
+    { name: 'User', value: `<@${user}> - ${user}` },
+    { name: 'Note', value: `${note}`},
+    { name: 'Severity', value: `${severity}`},
+    { name: 'Message Link', value: `${msgLink}` },
+    { name: 'Date Added', value: `<t:${Math.floor(newInteraction.createdTimestamp/1000)}:F>`},
+  )
+  .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+
+  const submitButton = new ButtonBuilder()
+    .setCustomId('submit')
+    .setLabel("Submit")
+    .setStyle(ButtonStyle.Success);
+  
+  // eslint-disable-next-line no-unused-vars
+  const editButton = new ButtonBuilder()
+    .setCustomId('edit')
+    .setLabel("Edit")
+    .setStyle(ButtonStyle.Primary);
+
+  const cancelButton = new ButtonBuilder()
+    .setCustomId('cancel')
+    .setLabel("Cancel")
+    .setStyle(ButtonStyle.Secondary);
+
+  const buttonRow = new ActionRowBuilder()
+    .addComponents(submitButton, cancelButton);
+
+  newInteraction.editReply({ephemeral:true, embeds: [noteEmbed], components: [buttonRow]});  
+
+  const collectorFilter = i => 
+    (i.user.id === interaction.user.id && 
+      (i.customId === "edit" || i.customId === "cancel" || i.customId === "submit" ));
+  
+  await newInteraction.awaitMessageComponent({ filter: collectorFilter, time: 60_000 })
+  .then(i => {
+    catchButton(newInteraction, i, data);
+  })
+}
+
+async function catchButton(interaction, newInteraction, data) {
+  let client = interaction.client;
+  await newInteraction.update({components:[]});
+  if (newInteraction.customId == 'submit'){
+
+    await submitNote(interaction, data);
+
+  } else if (newInteraction.customId == 'edit'){
+
+    await newInteraction.reply({ephemeral: true, content: "This feature has not been implemented yet"});
+
+  } else if (newInteraction.customId == 'cancel'){
+
+    let cancelEmbed = new EmbedBuilder()
+      .setTitle("Note cancelled")
+      .setDescription("Note add has been cancelled")
+      .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+    await newInteraction.reply({ephemeral: true, embeds:[cancelEmbed]})
+
+  } else {
+
+    let errorEmbed = new EmbedBuilder()
+          .setColor(0xff0000)
+          .setTitle("Command Error")
+          .setDescription("Something went very wrong")
+          .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+    interaction.client.error(`Received unknown button ID ${newInteraction.customId} in /staff note add`);
+    await newInteraction.reply({ephemeral: true, embeds:[errorEmbed]})
+
+  }
+}
+
+async function submitNote(interaction, data) {
+  let client = interaction.client;
+  let DB = client.DB;
+
+  try{
+
+  
+    await DB.Notes.create({
+      "User": data.user,
+      "Date": data.dateAdded,
+      "Note": data.note,
+      "Severity": data.severity,
+      "Link": data.msgLink,
+      "Noter": data.noter
+    });
+    console.log.info("Added note to database");
+
+  } catch (err){
+
+    client.log.error({message: "Failed to insert note into database", error: err})
+    let errorEmbed = new EmbedBuilder()
+          .setColor(0xff0000)
+          .setTitle("Command Error")
+          .setDescription("Something went very wrong")
+          .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+    await interaction.editReply({embeds: [errorEmbed]})
+
+  }
+  
+  let submitEmbed = new EmbedBuilder()
+          .setColor(0x00FF00)
+          .setTitle("Note added")
+          .setDescription("Your note has been stored")
+          .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+  await interaction.editReply({embeds: [submitEmbed]})
+
+}
 
 module.exports = {
   builder: function (SlashCommandBuilder){
@@ -32,7 +203,6 @@ module.exports = {
   execute: async function(interaction){
     const client = interaction.client;
 
-    var severity = interaction.options.getString('severity');
     var user = interaction.options.getUser('user');
     if (user == null){
         user = interaction.options.getString('userid');
@@ -46,58 +216,8 @@ module.exports = {
           await interaction.reply({ephemeral: true, embeds: [errorEmbed]});
           return;
         }
-    } else {
-      user = user.id;
-    }
+    } 
 
-
-    const modalID = crypto.randomBytes(16).toString("hex");
-    const modal = new ModalBuilder()
-      .setCustomId(modalID)
-      .setTitle("Enter note details");
-
-    const noteInput = new TextInputBuilder()
-      .setCustomId('note')
-      .setLabel("Note")
-      .setRequired(true)
-      .setStyle(TextInputStyle.Paragraph);
-    
-      const messageLinkInput = new TextInputBuilder()
-        .setCustomId('link')
-        .setLabel("Message Link")
-        .setValue("N/A")
-        .setRequired(false)
-        .setStyle(TextInputStyle.Paragraph);
-
-      const firstActionRow = new ActionRowBuilder().addComponents(noteInput);
-      const secondActionRow = new ActionRowBuilder().addComponents(messageLinkInput);
-
-      modal.addComponents(firstActionRow, secondActionRow);
-      await interaction.showModal(modal);
-
-      const collectorFilter = i => (i.user.id === interaction.user.id && i.customId === modalID);
-
-      interaction.awaitModalSubmit({ time: 60_000, collectorFilter })
-        .then(i => {
-          const note = i.fields.getTextInputValue('note');
-          const msgLink = i.fields.getTextInputValue('link');
-          
-          const noteEmbed = new EmbedBuilder()
-          .setColor(0xff0000)
-          .setTitle("Note added")
-          .setDescription("NOTE: This is a test, and the note is not actually stored yet")
-          .addFields(
-            { name: 'User', value: `<@${user}> - ${user}` },
-            { name: 'Note', value: `${note}`},
-            { name: 'Severity', value: `${severity}`},
-            { name: 'Message Link', value: `${msgLink}` },
-            { name: 'Date Added', value: `<t:${Math.floor(i.createdTimestamp/1000)}:F>`},
-          )
-          .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
-
-          i.reply({ephemeral:true, embeds: [noteEmbed]});
-        }).catch(err => {
-          console.log('No modal submit interaction was collected');
-        });      
+    await sendModal(interaction);
   }
 };
