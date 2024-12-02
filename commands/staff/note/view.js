ebf481412f9c15003a24dbc69d9b2de178bf015a
Alex Sweeney <x2110311x@gmail.com>
2024-12-01 21:12:18 -0500

    Fetch notes from database and display in embed

--- commands/staff/note/view.js
+++ commands/staff/note/view.js
@@ -1,3 +1,6 @@
+const { EmbedBuilder } = require('discord.js');
+
+
 module.exports = {
   builder: function (SlashCommandBuilder){
       SlashCommandBuilder.addSubcommand(subcommand =>
@@ -15,6 +18,46 @@ module.exports = {
       return SlashCommandBuilder;
   },
   execute: async function(interaction){
-      await interaction.reply("Command Response");
+    const client = interaction.client;
+
+    var user = interaction.options.getUser('user');
+    if (user == null){
+        user = interaction.options.getString('userid');
+        if (user == null){
+        let errorEmbed = new EmbedBuilder()
+        .setColor(0xff0000)
+        .setTitle("Command Error")
+        .setDescription("Please specify a user or user ID")
+        .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+
+        await interaction.reply({ephemeral: true, embeds: [errorEmbed]});
+        return;
+        }
+    }
+    
+    await interaction.deferReply();
+    
+    const notes = await client.DB.notes.findAll({
+        where: {
+            User: user
+        }
+    });
+
+    const guild = interaction.guild;
+    const member = await guild.members.fetch(`${user}`);
+    
+    const noteEmbed = new EmbedBuilder()
+        .setColor(0xffff88)
+        .setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL})
+        .setDescription("Please review, and then click Submit or Cancel");
+    
+    for (var note of notes){
+        noteEmbed.addFields({
+            'name': `${note.Severity} severity note submitted by <@${note.Noter}> on <t:${note.date}:F>`,
+            value: note.Note
+        });
+    }
+
+    await interaction.editReply({embeds: [noteEmbed]});
   }
 };
