95a7f976596f418e0eed3a77f0a1b0cfbb55dd62
Alex Sweeney <x2110311x@gmail.com>
2024-12-05 21:38:04 -0500

    Update DB schema

--- structures/DB/definition.sql
+++ structures/DB/definition.sql
@@ -4,8 +4,30 @@ CREATE TABLE `Notes` (
   `Date` bigint NOT NULL,
   `Note` text NOT NULL,
   `Severity` varchar(8) NOT NULL,
-  `Link` text NOT NULL,
+  `Link` text,
   `Noter` bigint NOT NULL
 );
 
+CREATE TABLE `PlaylistConfig` (
+  `ID` integer PRIMARY KEY AUTO_INCREMENT,
+  `Date` date NOT NULL,
+  `ThemeTitle` text NOT NULL,
+  `ThemeDescription` text,
+  `maxSubmissions` integer DEFAULT 2
+);
+
+CREATE TABLE `PlaylistData` (
+  `ID` integer PRIMARY KEY AUTO_INCREMENT,
+  `User` bigint NOT NULL,
+  `Track` text NOT NULL,
+  `Link` text NOT NULL,
+  `Reasoning` text,
+  `Picked` bool,
+  `Month` integer NOT NULL
+);
+
 CREATE INDEX `user_index` ON `Notes` (`User`);
+
+CREATE INDEX `month_index` ON `PlaylistData` (`Month`);
+
+ALTER TABLE `PlaylistData` ADD FOREIGN KEY (`Month`) REFERENCES `PlaylistConfig` (`ID`);
