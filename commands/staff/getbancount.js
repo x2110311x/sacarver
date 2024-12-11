3fe30bf023201211d400afb39c033604a43cebf4
Alex Sweeney <x2110311x@gmail.com>
2024-12-10 20:22:36 -0500

    We maxxed it out. have to do multiple fetches 😎😎😎

--- commands/staff/getbancount.js
+++ commands/staff/getbancount.js
@@ -1,5 +1,34 @@
 const { EmbedBuilder } = require('discord.js');
 
+async function fetchMoreBans(guild, limit = 10000) { // https://stackoverflow.com/a/72672522
+    if (!guild || typeof guild !== 'object')
+      throw new Error(`Expected a guild, got "${typeof guild}"`);
+    if (!Number.isInteger(limit))
+      throw new Error(`Expected an integer, got ${typeof limit}.`);
+    if (limit <= 1000) return guild.bans.fetch({ limit });
+  
+    let collection = new Collection();
+    let lastId = null;
+    let options = {};
+    let remaining = limit;
+  
+    while (remaining > 0) {
+      options.limit = remaining > 1000 ? 1000 : remaining;
+      remaining = remaining > 1000 ? remaining - 1000 : 0;
+  
+      if (lastId) options.before = lastId;
+  
+      let bans = await guild.bans.fetch(options);
+  
+      if (!bans.last()) break;
+  
+      collection = collection.concat(bans);
+      lastId = bans.last().id;
+    }
+  
+    return collection;
+  }
+
 module.exports = {
     builder: function (SlashCommandBuilder){
         SlashCommandBuilder.addSubcommand(subcommand =>
@@ -12,8 +41,7 @@ module.exports = {
     execute: async function(interaction){
         const guild = interaction.guild;
         let client = interaction.client;
-        const banManager = guild.bans;
-        const bans = await banManager.fetch({cache: false, force: true});
+        const bans = await fetchMoreBans(guild);
 
         const banEmbed = new EmbedBuilder()
         .setColor(0xDC8203)
