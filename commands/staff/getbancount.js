ece7ce3a561679d1d8d9c900cc3e077418c384b5
Alex Sweeney <x2110311x@gmail.com>
2024-12-10 20:17:29 -0500

    Add getbancount command lol

---
+++ commands/staff/getbancount.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    builder: function (SlashCommandBuilder){
+        SlashCommandBuilder.addSubcommand(subcommand =>
+            subcommand
+                .setName('getbancount')
+                .setDescription('Get the server ban count'));
+                    
+        return SlashCommandBuilder;
+    },
+    execute: async function(interaction){
+        const guild = interaction.guild;
+        const banManager = guild.bans;
+        const bans = await banManager.fetch({cache: false, force: true});
+
+        const banEmbed = new EmbedBuilder()
+        .setColor(0xDC8203)
+        .setTitle(`The server currently has ${bans.size} bans`)
+        .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
+        
+        await interaction.reply({embeds: [banEmbed]});
+
+};
