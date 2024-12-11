cb1e1c5728c786a49f70b40cac6b3d168e4342d4
Alex Sweeney <x2110311x@gmail.com>
2024-12-10 20:35:19 -0500

    Correct the function to not assume a total count at first. stackoverflow failed me

--- commands/staff/getbancount.js
+++ commands/staff/getbancount.js
@@ -1,29 +1,24 @@
 const { Collection, EmbedBuilder } = require('discord.js');
 
-async function fetchMoreBans(guild, limit = 10000) { // https://stackoverflow.com/a/72672522
+async function fetchMoreBans(guild) { // https://stackoverflow.com/a/72672522
     if (!guild || typeof guild !== 'object')
       throw new Error(`Expected a guild, got "${typeof guild}"`);
-    if (!Number.isInteger(limit))
-      throw new Error(`Expected an integer, got ${typeof limit}.`);
-    if (limit <= 1000) return guild.bans.fetch({ limit });
   
     let collection = new Collection();
     let lastId = null;
-    let options = {};
-    let remaining = limit;
+    let count = 1000;
+    let bans = null;
+
+    while (count >= 1000) {
+        if (lastId){
+            bans = await guild.bans.fetch({limit: 1000, before:lastId, cache: false, force: true});
+        } else {
+            bans = await guild.bans.fetch({limit: 1000, cache: false, force: true});
+        }
+        count = bans.size();
   
-    while (remaining > 0) {
-      options.limit = remaining > 1000 ? 1000 : remaining;
-      remaining = remaining > 1000 ? remaining - 1000 : 0;
-  
-      if (lastId) options.before = lastId;
-  
-      let bans = await guild.bans.fetch(options);
-  
-      if (!bans.last()) break;
-  
-      collection = collection.concat(bans);
-      lastId = bans.last().id;
+        collection = collection.concat(bans);
+        lastId = bans.last().id;
     }
   
     return collection;
