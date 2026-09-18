1a67cbd839d2fea3efd13987e8b09077fed66f41
Alex Sweeney <x2110311x@gmail.com>
2026-09-18 01:48:48 +0000

    add minimum value constraint to sides option in roll command

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,35 +1,36 @@
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    builder: function (SlashCommandBuilder){
+    builder: function (SlashCommandBuilder) {
         SlashCommandBuilder.addSubcommand(subcommand =>
             subcommand
                 .setName('roll')
                 .setDescription('Roll a die of any size!')
-                .addIntegerOption(option => 
+                .addIntegerOption(option =>
                     option
                         .setName("sides")
+                        .setMinValue(0)
                         .setDescription("How many sides should the die have. Defaults to 6")));
         return SlashCommandBuilder;
     },
-    execute: async function(interaction){
+    execute: async function (interaction) {
         const preRoll = new EmbedBuilder()
             .setColor(0xd5b052)
             .setTitle("Please wait while the die is rolled")
             .setImage('https://c.tenor.com/HcK7RSiai-AAAAAi/dice-roll-dice.gif')
             .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${interaction.client.icon}` });
-        
-        await interaction.reply({embeds: [preRoll]});
+
+        await interaction.reply({ embeds: [preRoll] });
         let sides = interaction.options.getInteger('sides') ?? 6;
         let result = Math.floor(Math.random() * sides);
-        
+
         const postRoll = new EmbedBuilder()
             .setColor(0xd5b052)
             .setTitle(`On a ${sides}-sided die, you rolled a...`)
             .setDescription(`${result}`)
             .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${interaction.client.icon}` });
-        
+
         await new Promise(resolve => setTimeout(resolve, 4000));
-        await interaction.editReply({embeds: [postRoll]});
+        await interaction.editReply({ embeds: [postRoll] });
     }
 };
