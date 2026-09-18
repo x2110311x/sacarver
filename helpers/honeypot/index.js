3bb3e373b0fad83bcda6e19da761c00c7693640a
Alex Sweeney <x2110311x@gmail.com>
2026-09-18 01:33:09 +0000

    add warning message to honeypot channel topic

--- helpers/honeypot/index.js
+++ helpers/honeypot/index.js
@@ -1,10 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
 
 
-async function processHoneypot(client, message){
+async function processHoneypot(client, message) {
   if (message.member.roles.cache.some((role) => role.id == client.config.roles.staff)) {
     client.log.info(`Staff member ${message.author.tag} sent message in honeypot channel, ignoring.`);
-	  return;
+    return;
   }
 
   var data = {
@@ -12,19 +12,19 @@ async function processHoneypot(client, message){
     "severity": "high",
     "note": "User sent a message in the honeypot channel.",
     "message": message.content,
-    "dateAdded": Math.floor(message.createdTimestamp/1000),
+    "dateAdded": Math.floor(message.createdTimestamp / 1000),
     "noter": client.config.clientID,
     "DMd": false,
     "banned": false
   }
 
   // DM the user
-  try{
+  try {
     var dmChannel = await message.author.createDM();
     await dmChannel.send("You have been banned from \`twenty one pilots\` for sending a message in our honeypot channel.\nThis channel is designed to automatically combat scam messages. If you believe this was a mistake, please submit a ban appeal at https://bans.discordclique.com");
     data.DMd = true;
-  } catch (err){
-    client.log.error({message: "Failed to DM user about honeypot ban", error: err})
+  } catch (err) {
+    client.log.error({ message: "Failed to DM user about honeypot ban", error: err })
   }
 
   // Ban the user
@@ -33,8 +33,8 @@ async function processHoneypot(client, message){
     await member.ban({ deleteMessageSeconds: 300, reason: 'User sent message in honeypot channel.' });
     data.banned = true;
   }
-  catch (err){
-    client.log.error({message: "Failed to ban user for honeypot detection", error: err})
+  catch (err) {
+    client.log.error({ message: "Failed to ban user for honeypot detection", error: err })
   }
   await message.delete();
   await staffLog(client, data);
@@ -98,7 +98,7 @@ async function updateHoneypotChannel(client, count) {
   try {
     const honeypotChannel = await client.channels.fetch(client.config.channels.honeypot);
     if (honeypotChannel && typeof honeypotChannel.setTopic === 'function') {
-      const newTopic = `Scammers Banned: ${count}`;
+      const newTopic = `⚠️ 🍯 DO NOT SEND MESSAGES HERE 🍯 ⚠️\n Scammers Banned: ${count}`;
       await honeypotChannel.setTopic(newTopic);
       client.log.info(`Updated honeypot channel topic to "${newTopic}"`);
     }
@@ -107,31 +107,31 @@ async function updateHoneypotChannel(client, count) {
   }
 }
 
-async function staffLog(client, data){
-    const banLogEmbed = new EmbedBuilder()
+async function staffLog(client, data) {
+  const banLogEmbed = new EmbedBuilder()
     .setColor(0xff0000)
     .setTitle("User sent message in honeypot channel")
     .setDescription(`${data.DMd ? "User was DM'd ban appeal" : "User was unable to be DM'd."}\n User ${data.banned ? "was" : "was unable to be"} automatically banned for honeypot detection.`)
     .addFields(
-      { name: 'User', value: `<@${data.user}> - ${data.user}`},
+      { name: 'User', value: `<@${data.user}> - ${data.user}` },
       { name: 'Message Text', value: `${(data.message == "") ? "No text content" : data.message}` },
-      { name: 'Date banned', value: `<t:${data.dateAdded}:F>`}
+      { name: 'Date banned', value: `<t:${data.dateAdded}:F>` }
     )
     .setFooter({ text: `© ${new Date().getFullYear()} x2110311x`, iconURL: `${client.icon}` });
 
-    const banLog = await client.channels.fetch(client.config.channels.banLog);
-    const chatModeration = await client.channels.fetch(client.config.channels.chatModeration);
-    
-    if(data.banned){
-      await banLog.send({ embeds: [banLogEmbed]});
-    }
-    await chatModeration.send({ embeds: [banLogEmbed]});
+  const banLog = await client.channels.fetch(client.config.channels.banLog);
+  const chatModeration = await client.channels.fetch(client.config.channels.chatModeration);
+
+  if (data.banned) {
+    await banLog.send({ embeds: [banLogEmbed] });
+  }
+  await chatModeration.send({ embeds: [banLogEmbed] });
 }
 
 async function addNote(client, data) {
   let DB = client.DB;
   data.note += `\nUser ${data.banned ? "was" : "was unable to be"} automatically banned.`;
-  try{
+  try {
     await DB.Notes.create({
       "User": data.user,
       "Date": data.dateAdded,
@@ -139,11 +139,11 @@ async function addNote(client, data) {
       "Severity": data.severity,
       "Noter": data.noter
     });
-  
-  client.log.info("Added note to database");
-  } catch (err){
 
-    client.log.error({message: "Failed to insert note into database", error: err})
+    client.log.info("Added note to database");
+  } catch (err) {
+
+    client.log.error({ message: "Failed to insert note into database", error: err })
     return;
   }
 }
